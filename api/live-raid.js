974f7b3bc3b422d2190be4eb91626efc1cf6187c
Asgardt86 <sammy.maddour@startmail.com>
2026-03-09 20:35:55 +0100

    Update live-raid.js

--- api/live-raid.js
+++ api/live-raid.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
       .from(`${clientId}:${clientSecret}`)
       .toString("base64");
 
-    // Token holen
+    // OAuth Token holen
     const tokenResponse = await fetch(
       "https://www.warcraftlogs.com/oauth/token",
       {
@@ -25,44 +25,84 @@ export default async function handler(req, res) {
     const tokenData = await tokenResponse.json();
     const accessToken = tokenData.access_token;
 
-
-    // GraphQL Query
-const reportCode =
-reportsData.data.reportData.reports.data[0].code;
-
-const fightsQuery = `
-{
-  reportData {
-    report(code: "${reportCode}") {
-      fights {
-        id
-        name
-        bossPercentage
-        kill
+    // neuesten Report der Gilde holen
+    const reportsQuery = `
+      {
+        reportData {
+          reports(
+            guildName: "We Pull at Two",
+            guildServerSlug: "blackrock",
+            guildServerRegion: "eu",
+            limit: 1
+          ) {
+            data {
+              code
+              title
+              startTime
+              endTime
+            }
+          }
+        }
       }
-    }
-  }
-}
-`;
+    `;
 
-const fightsResponse = await fetch(
-  "https://www.warcraftlogs.com/api/v2/client",
-  {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({ query: fightsQuery })
-  }
-);
+    const reportsResponse = await fetch(
+      "https://www.warcraftlogs.com/api/v2/client",
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ query: reportsQuery })
+      }
+    );
 
-const fightsData = await fightsResponse.json();
+    const reportsData = await reportsResponse.json();
 
-res.status(200).json({
-  report: reportCode,
-  fights: fightsData.data.reportData.report.fights
-});
+    const reportCode =
+      reportsData.data.reportData.reports.data[0].code;
+
+    // Fight Daten laden
+    const fightsQuery = `
+      {
+        reportData {
+          report(code: "${reportCode}") {
+            fights {
+              id
+              name
+              bossPercentage
+              kill
+            }
+          }
+        }
+      }
+    `;
+
+    const fightsResponse = await fetch(
+      "https://www.warcraftlogs.com/api/v2/client",
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ query: fightsQuery })
+      }
+    );
+
+    const fightsData = await fightsResponse.json();
+
+    res.status(200).json({
+      report: reportCode,
+      fights: fightsData.data.reportData.report.fights
+    });
+
+  } catch (error) {
+
+    res.status(500).json({
+      error: error.message
+    });
 
   }
 
