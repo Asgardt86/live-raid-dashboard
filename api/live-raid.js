62c8296dc0377aab890fac33e458276eeeb4ce14
Asgardt86 <sammy.maddour@startmail.com>
2026-03-09 20:11:24 +0100

    Update live-raid.js

--- api/live-raid.js
+++ api/live-raid.js
@@ -1,8 +1,30 @@
 export default async function handler(req, res) {
+  try {
+    const clientId = process.env.WCL_CLIENT_ID;
+    const clientSecret = process.env.WCL_CLIENT_SECRET;
 
-  res.status(200).json({
-    clientId: process.env.WCL_CLIENT_ID,
-    secretLength: process.env.WCL_CLIENT_SECRET?.length
-  });
+    const params = new URLSearchParams();
+    params.append("grant_type", "client_credentials");
+    params.append("client_id", clientId);
+    params.append("client_secret", clientSecret);
 
+    const tokenResponse = await fetch("https://www.warcraftlogs.com/oauth/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      body: params.toString()
+    });
+
+    const tokenData = await tokenResponse.json();
+
+    res.status(200).json({
+      tokenData
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      error: error.message
+    });
+  }
 }
